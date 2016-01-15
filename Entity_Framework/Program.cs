0d7803547d57f9470f643b9e1b1a944106b96a4a
mikemajesty <mikemajesty2011@gmail.com>
2016-01-14 23:06:28 -0200

    Part 1: Introduction to Entity Framework​ [55:52]

---
+++ Entity_Framework/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using static System.Console;
+
+namespace Entity_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MusicContext())
+            {
+                var count = context.Album.Count();
+
+                WriteLine(count);
+                WriteLine(context.Database.Connection.ConnectionString);
+                context.Album.Add(new Album
+                {
+                    Price = 9.99m,
+                     Title = "Wish"
+                });               
+                context.SaveChanges();
+                var newCount = context.Album.Count();
+                WriteLine(newCount);
+                var album = context.Album.Where(c=>c.Title == "Wish");
+                ReadKey();
+            }
+        }
+
+     
+
+    }
+    [Table(nameof(Album))]
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public decimal Price { get; set; }
+        public String Title { get; set; }
+    }
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Album { get; set; }
+    }
+}
